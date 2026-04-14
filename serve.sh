c0c39c5678abc681abaf70b5f840cf1438befd66
Kevin Kennedy <kevkenne@amazon.com>
2026-04-14 16:12:34 +1200

    chore: remove deployment scripts from tracking for aws-samples readiness

--- serve.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "==> Serving frontend at http://localhost:8080"
-echo "    Open http://localhost:8080 in your browser"
-echo "    Press Ctrl+C to stop"
-cd "$(dirname "$0")/frontend"
-python3 -m http.server 8080
